bee07eeb28e8c58daa30a87715ddcfc868d1edc8
Your Name <you@example.com>
2025-12-06 10:42:28 +0200

    Remove sample directories and files

--- samples/palestine-shield-main/src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Login from './Login';
-
-const Index = () => {
-  return <Login />;
-};
-
-export default Index;
